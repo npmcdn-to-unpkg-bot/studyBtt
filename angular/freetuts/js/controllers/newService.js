879d851b244c9a79eeccc867b8d2cc1513b2e4bd
truongbt <truongbt@atmarkcafe.org>
2016-01-11 14:14:35 +0700

    push commit

--- angular/freetuts/js/controllers/newService.js
+++ angular/freetuts/js/controllers/newService.js
@@ -1,3 +1,9 @@
 /**
  * Created by MSI on 11-Jan-16.
  */
+app.controller('newService', ['$scope', 'kt_so_chan', function ($scope, kt_so_chan){
+    $scope.thongbao = function () {
+        kt_so_chan($scope.number);
+    }
+
+}]);
