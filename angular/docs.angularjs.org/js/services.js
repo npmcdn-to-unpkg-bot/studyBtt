23df7a46ae18dd0f30bfc8c04c80e2ec3e374d5a
truongbt <d10cn2btt@gmail.com>
2016-01-15 17:56:49 +0700

    step 11

--- angular/docs.angularjs.org/js/services.js
+++ angular/docs.angularjs.org/js/services.js
@@ -2,3 +2,12 @@
 
 /* Services */
 
+var phoneCatServices = angular.module('phoneCatServices', ['ngResource']);
+
+phoneCatServices.factory('Phone', ['$resource',
+    function ($resource) {
+        return $resource('phones/:phoneId.json', {}, {
+            query: {method : 'GET', params : {phoneId : 'phones'}, isArray : true}
+        });
+    }
+])
