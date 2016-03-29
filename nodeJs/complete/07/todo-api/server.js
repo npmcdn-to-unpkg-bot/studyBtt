cf2875ad1aea716af50e14240d0d4ccabfebd9fd
truongbt <d10cn2btt@gmail.com>
2016-03-29 13:44:11 +0700

    init repo

--- nodeJs/complete/07/todo-api/server.js
+++ nodeJs/complete/07/todo-api/server.js
@@ -1,3 +1,14 @@
 /**
  * Created by MSI on 21-Mar-16.
  */
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+    res.send('Todo API Root !');
+});
+
+app.listen(PORT, function () {
+    console.log('Server started in port ' + PORT);
+});
