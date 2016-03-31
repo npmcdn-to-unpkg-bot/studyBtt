7b6f8cd5760455976ec941005263acec6bd3b57f
truongbt <d10cn2btt@gmail.com>
2016-03-31 14:48:27 +0700

    api post

--- nodeJs/complete/07/todo-api/index.js
+++ nodeJs/complete/07/todo-api/index.js
@@ -2,17 +2,13 @@
  * Created by MSI on 21-Mar-16.
  */
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos =[{
-    id: 1,
-    description: 'Have lunch',
-    completed: false
-}, {
-    id: 2,
-    description: 'Swimming',
-    completed: true
-}];
+var todos =[];
+var todoNextId = 0;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('Todo API Root !');
@@ -39,6 +35,16 @@ app.get('/todos/:id', function (req, res) {
     } else {
         res.status(404).send();
     }
+});
+
+//POST /todo
+app.post('/todos', function (req, res) {
+    var body = req.body;
+    //add id field
+    body.id = todoNextId++;
+    //push body into array
+    todos.push(body);
+    res.json(body);
 });
 app.listen(PORT, function () {
     console.log('Server started in port ' + PORT);
