27175c055077e40993549021064266dcacc30feb
d10cn2btt <d10cn2btt@gmail.com>
2016-05-20 00:37:39 +0700

    master layout

---
+++ php/qhonline/ci/application/controllers/MasterLayout.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Truong
+ * Date: 20-May-16
+ * Time: 12:12 AM
+ */
+class MasterLayout
+{
+    
+}
---
+++ php/qhonline/ci/application/views/masterlayout/index_view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Truong
+ * Date: 20-May-16
+ * Time: 12:15 AM
+ */
