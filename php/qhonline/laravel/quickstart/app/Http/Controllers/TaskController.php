02386d45369707b963d606e72e39af7c963b0def
truongbt <d10cn2btt@gmail.com>
2016-07-14 17:59:06 +0700

    create task controller

--- php/qhonline/laravel/quickstart/app/Http/Controllers/TaskController.php
+++ php/qhonline/laravel/quickstart/app/Http/Controllers/TaskController.php
@@ -8,5 +8,39 @@ use App\Http\Requests;
 
 class TaskController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a list of all of the user's task.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+        return view('tasks.index');
+    }
+
+    /**
+     * Create a new task.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+
+        // Create The Task...
+    }
 }
