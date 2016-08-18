c65eca782cf690db06a9b0c598e8f762fbe0f2ae
truongbt <d10cn2btt@gmail.com>
2016-08-18 23:45:15 +0700

    add route

---
+++ angular2/router/app/app.routing.ts
@@ -0,0 +1,37 @@
+import { Routes, RouterModule }   from '@angular/router';
+
+import { loginRoutes,
+         authProviders }  from './login.routing';
+
+import { CanDeactivateGuard } from './can-deactivate-guard.service';
+
+const crisisCenterRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  },
+  {
+    path: 'crisis-center',
+    loadChildren: 'app/crisis-center/crisis-center.module#CrisisCenterModule'
+  }
+];
+
+const appRoutes: Routes = [
+  ...loginRoutes,
+  ...crisisCenterRoutes
+];
+
+export const appRoutingProviders: any[] = [
+  authProviders,
+  CanDeactivateGuard
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
